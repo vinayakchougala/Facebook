0ce8d40f6fe6ddcaa7a03daf9f0225f5e4070668
vinayakchougala <vinayakac93@gmail.com>
2023-07-13 13:00:11 +0530

    commit 3

--- Facebook/src/test/java/Punter/LogintoAppTest.java
+++ Facebook/src/test/java/Punter/LogintoAppTest.java
@@ -1,20 +1,25 @@
 package Punter;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LogintoAppTest 
 {
-   public static void main(String[] args)
+   @Test
+   public void tc1()
    {
-	  
-	 System.out.println("HI");
-	 System.out.println("HELLO");
-	 System.out.println("HI");
-	 System.out.println("HELLO");
-	 
+	   
+	   WebDriver driver = new ChromeDriver();
+		 driver.get("https://www.amazon.in");
+		 driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		 
+		 driver.manage().window().maximize();
+		 driver.close();
 }
 	
 }
