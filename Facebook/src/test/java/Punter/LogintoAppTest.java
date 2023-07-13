016dea3e7f7613baa3e5fb19f19e0bcb600b5aec
vinayakchougala <vinayakac93@gmail.com>
2023-07-13 10:32:02 +0530

    Commit 1

---
+++ Facebook/src/test/java/Punter/LogintoAppTest.java
@@ -0,0 +1,21 @@
+package Punter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LogintoAppTest 
+{
+   public static void main(String[] args)
+   {
+	  
+	 WebDriver driver= new ChromeDriver();
+	 driver.get("https://www.amazon.in");
+	 driver.manage().window().maximize();
+	 driver.close();
+	 
+}
+	
+}
+	
